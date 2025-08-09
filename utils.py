2a3f38f14ed083d0e918dfd3a2afdc02465f370e
Durgesh Rai <durgeshraihero@gmail.com>
2025-08-09 17:09:44 +0530

    Remove all tracked files

--- utils.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-def encode_uid(user_id: int) -> str:
-    return base64.urlsafe_b64encode(str(user_id).encode()).decode()
-
-def decode_uid(encoded: str) -> int:
-    return int(base64.urlsafe_b64decode(encoded.encode()).decode())
